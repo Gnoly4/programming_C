283c2bcdf45e1d64f17b5e0f8aa0047282e3f554
Gnoly4 <den2006455@gmail.com>
2025-03-01 13:43:02 +1000

    Add files via upload

--- laba03/main.c
+++ laba03/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    char str[80];
+    int i = 0;
+
+    printf("vedi strochku: ");
+    gets(str);
+
+    while (str[i] != '\0') {
+        if (str[i] == 'a' || str[i] == 'b') {
+            str[i] = str[i] - 32;
+        }
+        i++;
+    }
+
+    printf("result: %s\n", str);
+    return 0;
+}
