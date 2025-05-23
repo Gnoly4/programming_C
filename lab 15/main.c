207fd0cda4c74a665834b34e2c44fb069c95cc11
Gnoly4 <den2006455@gmail.com>
2025-05-23 08:16:24 +0300

    Add files via upload

--- lab 15/main.c
+++ lab 15/main.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+
+void print_digits_forward(unsigned int n) {
+    if (n == 0) return;
+    print_digits_forward(n / 10);
+    printf("%u ", n % 10);
+}
+
+void print_digits_backward(unsigned int n) {
+    if (n == 0) return;
+    printf("%u ", n % 10);
+    print_digits_backward(n / 10);
+}
+
+int main() {
+    unsigned int N;
+
+    printf("Enter a natural number N: ");
+    if (scanf("%u", &N) != 1 || N == 0) {
+        printf("Error: enter the number that higher than zero.\n");
+        return 1;
+    }
+
+    if (N == 0) {
+        printf("0\n0\n");
+        return 0;
+    }
+
+    print_digits_forward(N);
+    printf("\n");
+
+    print_digits_backward(N);
+    printf("\n");
+
+    return 0;
+}
 
